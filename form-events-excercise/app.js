7d7500858e6965fbfd87b5aa2b4a0a46aaaa351e
danilo-cupido <danilocupido@gmail.com>
2023-07-05 20:56:04 +0100

    add alternative solutions

--- form-events-excercise/app.js
+++ form-events-excercise/app.js
@@ -14,6 +14,41 @@ form.addEventListener('submit', function (e) {
 
 const addProduct = (product, quantity) => {
 	const newLI = document.createElement('li');
-	newLI.innerText = `${product} ${quantity}`;
+	newLI.innerText = `${quantity} ${product}`;
 	list.insertAdjacentElement('beforeend', newLI);
 };
+
+// SOLUTION #2:
+
+// const form = document.querySelector('form');
+// const ul = document.querySelector('#list');
+
+// form.addEventListener('submit', function (e) {
+//   e.preventDefault();
+//   const qty = form.elements.qty.value;
+//   const product = form.elements.product.value;
+//   const li = document.createElement('li');
+//   li.innerText = `${qty} ${product}`;
+//   ul.appendChild(li);
+//   form.reset();
+// });
+
+// SOLUTION #3 :
+
+// const form = document.querySelector('form');
+// const list = document.querySelector('#list');
+
+// form.addEventListener('submit', function (e) {
+//   e.preventDefault();
+//   const productInput = form.elements.product;
+//   const quantityInput = form.elements.qty;
+//   addElement(productInput.value, quantityInput.value);
+//   productInput.value = '';
+//   quantityInput.value = '';
+// });
+
+// const addElement = (product, qty) => {
+//   const li = document.createElement('li');
+//   li.innerText = `${qty} ${product}`;
+//   list.appendChild(li);
+// }
