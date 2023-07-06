76fd09ba85de40869377edb9ef47217165b8a759
danilo-cupido <danilocupido@gmail.com>
2023-07-06 12:26:55 +0100

    Score Selector basic logic

--- score-keeper/app.js
+++ score-keeper/app.js
@@ -2,6 +2,8 @@ const p1Button = document.querySelector('#p1Button');
 const p2Button = document.querySelector('#p2Button');
 const p1Display = document.querySelector('#p1Display');
 const p2Display = document.querySelector('#p2Display');
+const resetButton = document.querySelector('#reset');
+const winningScoreSelect = document.querySelector('#playto');
 
 let p1Score = 0;
 let p2Score = 0;
@@ -26,3 +28,17 @@ p2Button.addEventListener('click', function () {
 		p2Display.textContent = p2Score;
 	}
 });
+
+winningScoreSelect.addEventListener('change', function () {
+	winningScore = parseInt(this.value);
+});
+
+resetButton.addEventListener('click', reset);
+
+function reset() {
+	isGameOver = false;
+	p1Score = 0;
+	p2Score = 0;
+	p1Display.textContent = 0;
+	p2Display.textContent = 0;
+}
