6ed1d0b4e45eeea83340da64003a6da05f3ca6fc
danilo-cupido <danilocupido@gmail.com>
2023-07-06 18:08:41 +0100

    clean up

--- score-keeper/app.js
+++ score-keeper/app.js
@@ -8,16 +8,9 @@ const p2 = {
 	button: document.querySelector('#p2Button'),
 	display: document.querySelector('#p2Display'),
 };
-
-// const p1Button = document.querySelector('#p1Button');
-// const p2Button = document.querySelector('#p2Button');
-// const p1Display = document.querySelector('#p1Display');
-// const p2Display = document.querySelector('#p2Display');
 const resetButton = document.querySelector('#reset');
 const winningScoreSelect = document.querySelector('#playto');
 
-// let p1Score = 0;
-// let p2Score = 0;
 let winningScore = 3;
 let isGameOver = false;
 
